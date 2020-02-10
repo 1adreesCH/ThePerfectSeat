bf885682d54be6e6ac129082bf1a959fcb39468c
Reorso <reorso@hotmail.it>
2020-02-10 17:25:35 +0000

    Added first prototype project and build
    Added first prototype project and build

---
+++ Game/UnityProjects/Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float minDistance;
+    bool seated = false;
+    Vector3 prevPos;
+    Vector3 temp;
+    public string[] axis = new string[2];
+    public KeyCode seat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = GetComponent<Rigidbody>().velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //temp = GetComponent<Rigidbody>().velocity;
+        temp.x = Input.GetAxis(axis[0]);
+        temp.z = Input.GetAxis(axis[1]);
+        temp.y = 0;
+        transform.forward = temp;
+
+        if (!seated)
+        {
+            temp = temp.normalized * speed * Time.deltaTime;
+            GetComponent<Rigidbody>().velocity = temp;
+        }
+       if (Input.GetKeyDown(seat))
+        {
+            if ((transform.position - target.position).magnitude < minDistance)
+            {
+                prevPos = transform.position;
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                transform.position = target.position + Vector3.up;
+                seated = true;
+            }
+        }
+        if (Input.GetKeyUp(seat))
+        {
+            if (seated)
+            {
+                seated = false;
+                transform.position = prevPos;
+            }
+
+        }
+        //if (Input.GetKeyDown(seat))
+        //{
+        //    if (seated)
+        //    {
+        //        transform.position = prevPos;
+        //        seated = false;
+        //    }
+        //    else if((transform.position - target.position).magnitude < minDistance)
+        //    {
+        //        prevPos = transform.position;
+        //        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //        transform.position = target.position + Vector3.up;
+        //        seated = true;
+        //    }
+        //}
+    }
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Seat")
+        {
+            if (target != null)
+            {
+                target.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+            }
+            print(col.name);
+            target = col.transform;
+            col.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+    }
+    void onTriggerStay()
+    {
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        target.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+    }
+
+}
---
+++ Game/UnityProjects/Prototype1/Assets/Scripts/Randomizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour
+{
+    public Renderer[] cubes;
+    Renderer selected;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(0, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            randomize();
+            timer = Random.Range(5, 10);
+        }
+    }
+
+    void randomize()
+    {
+        if(selected != null)
+        {
+            selected.material.SetColor("_Color", Color.white);
+        }
+        selected = cubes[Random.Range(0, cubes.Length)];
+        selected.material.SetColor("_Color", Color.black);
+    }
+}
