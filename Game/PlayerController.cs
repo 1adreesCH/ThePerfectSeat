8c270d19684b5b3c6b55170ff8fe10e32f6507d5
DrewMcV1 <44735147+DrewMcV1@users.noreply.github.com>
2020-02-29 23:19:54 +0000

    Made a basic behaviour script that isn't quite working yet

---
+++ Game/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float sprintSpeed;
+    public float minDistance;
+    bool seated = false;
+    Vector3 prevPos;
+    Vector3 temp;
+    public string[] axis = new string[2];
+    public KeyCode seat;
+    public KeyCode sprint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = GetComponent<Rigidbody>().velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //temp = GetComponent<Rigidbody>().velocity;
+        temp.x = Input.GetAxis(axis[0]);
+        temp.z = Input.GetAxis(axis[1]);
+        temp.y = 0;
+        transform.forward = temp;
+
+        if (!seated)
+        {
+            temp = temp.normalized * speed * Time.deltaTime;
+            GetComponent<Rigidbody>().velocity = temp;
+        }
+       if (Input.GetKeyDown(seat))
+        {
+            if ((transform.position - target.position).magnitude < minDistance)
+            {
+                prevPos = transform.position;
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                transform.position = target.position + Vector3.up;
+                seated = true;
+
+                target.tag = "Untagged"; //removes tag from the chair the player is currently sitting on. 
+            }
+        }
+        if (Input.GetKeyUp(seat))
+        {
+            if (seated)
+            {
+                seated = false;
+                transform.position = prevPos;
+                
+                target.tag = "Seat"; //adds tag to the chair the player is currently sitting on.
+            }
+
+        }
+        //if (Input.GetKeyDown(seat))
+        //{
+        //    if (seated)
+        //    {
+        //        transform.position = prevPos;
+        //        seated = false;
+        //    }
+        //    else if((transform.position - target.position).magnitude < minDistance)
+        //    {
+        //        prevPos = transform.position;
+        //        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //        transform.position = target.position + Vector3.up;
+        //        seated = true;
+        //    }
+        //}
+
+        if (Input.GetKeyDown(sprint))
+        {
+            speed = sprintSpeed;
+
+        }
+
+        if (Input.GetKeyUp(sprint))
+        {
+            speed = 600f; 
+        }
+    }
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Seat")
+        {
+            if (target != null)
+            {
+                target.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+            }
+            print(col.name);
+            target = col.transform;
+            col.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+    }
+    void onTriggerStay()
+    {
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        target.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+    }
+
+}
---
+++ Game/UnityProjects/ThePerfectSeat-Main/Assets/Scripts/BehaviourScript.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScript : MonoBehaviour
+{
+    int i;
+    GameObject[] seats;
+    float timePassed = 0;
+    int angerCounter;
+
+    //public GameObject player2;
+    //public GameObject player1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seats = GameObject.FindGameObjectsWithTag("Seat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i])
+            {
+
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        timePassed += Time.deltaTime;
+
+        //the idea here is to have a set amount of time where the crowd person gets more and more angry to the point
+        //where they perform an action.
+        while (angerCounter < 3)
+        {
+            if (col.gameObject.tag.Equals("Player") && timePassed >= 6.0f)
+            {
+                Debug.Log("Getting angry");
+                timePassed = 0;
+                angerCounter++;
+            }
+        }
+
+        Debug.Log("Anger max");
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        angerCounter = 0;
+    }
+
+
+
+}
