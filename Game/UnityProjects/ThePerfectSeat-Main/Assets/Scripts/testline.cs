d3978b11d2925a7acf547d20dc39ba82351d387c
1adreesCH <adreeschaudhari@gmail.com>
2020-03-01 06:39:53 +0000

    u

--- Game/UnityProjects/ThePerfectSeat-Main/Assets/Scripts/testline.cs
+++ Game/UnityProjects/ThePerfectSeat-Main/Assets/Scripts/testline.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class testline : MonoBehaviour
 {
+
+    public GameObject cube;
+    //private Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //lineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        //lineRenderer.transform.localScale=(0f,0f,1f);
+        //lineRenderer.SetWidth(0f, 0f);
+        //lineRenderer.startWidth(0f,0f);
+        //lineRenderer.endWidth(0f);
+        //lineRenderer.transform.localScale(1, 1, 1);
+        //gameObject.GetComponent<LineRenderer>().startWidth(2.0f, 1.5f);
+        cube.transform.localScale(1, 1, 1);
+        //scoreText.GetComponent<Text>().text = "Score:" + score;
+        //scaleChange = new Vector3(0f, 0f, 0f);
+        Vector3 = Vector3(1,1,1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //LineRenderer.transform.localScale += scaleChange;
+       // sphere.transform.localScale += scaleChange;
         
     }
 }
